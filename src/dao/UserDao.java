aac9eafe4d059c3ae6bce754400f7e5a3d684372
SulaimanovaAigera <167988792+SulaimanovaAigera@users.noreply.github.com>
2024-07-04 18:37:22 +0600

    Revert "Feature/create user methods"

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -6,10 +6,4 @@ import java.util.List;
 
 public interface UserDao {
 
-    User addUser(User user);
-    User getUserById(Long id);
-    String updateUser(Long id, User user);
-    List<User>getAllUser();
-    String deleteUser(Long id);
-    User getUserByEmail(String userEmail);
 }
